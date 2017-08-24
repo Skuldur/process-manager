63a2beb6e14d08356b895069fd03234b9075fc02
skuldur <siggiskuli@gmail.com>
2017-08-24 22:15:26 +0000

    Removed init command
    Removed init command for auto-initialization functionality

--- src/js/cli-interface.js
+++ src/js/cli-interface.js
@@ -4,70 +4,102 @@ const fs = require('fs');
 const colors = require('colors');
 const kill = require('tree-kill');
 
-const client = io.connect('http://localhost');
+let client;
 const out = fs.openSync(`${__dirname}/../logs/process-manager.log`, 'a');
 const err = fs.openSync(`${__dirname}/../logs/process-manager.log`, 'a');
 
-function init() {
+function verifyServerAvailability(cb) {
+  client = io.connect('http://localhost');
+
+  client.on('connect_error', function() {
+    init(cb);
+  });
+
+  client.on('connect', function () {
+    cb();
+  });
+
+  client.on('messageReceived', () => {
+    client.disconnect();
+    process.exit(0);
+  });
+}
+
+function init(cb) {
   /* 
     We try killing the process manager in case the user runs the init
     while another process-manager is running. This would cause a runaway
     process-manager.
   */
   killProcessManager(() => {
-    const child = spawn('node', [`${__dirname}//ProcessManager/process-interface.js`], {
+    const child = spawn('node', [`${__dirname}/ProcessManager/process-interface.js`], {
       detached: true,
       stdio: ['ignore', out, err],
     });
 
     fs.writeFileSync(`${__dirname}/../data/processrunnerId.txt`, child.pid, { flags: 'a' });
 
-    process.exit(0);
+    cb();
   })
 }
 
 function add(file) {
-  const processes = require(file);
-  client.emit('add', processes);
+  verifyServerAvailability(() => {
+    const processes = require(`${process.cwd()}/${file}`);
+    client.emit('add', processes);
 
-  client.on('processExists', (processId) => {
-    console.log(`Process `, colors.cyan(processId.toUpperCase()), " already exists");
+    client.on('processExists', (processId) => {
+      console.log(`Process `, colors.cyan(processId.toUpperCase()), " already exists");
+    });
   });
+  
 }
 
 function remove(id) {
-  client.emit('remove', id);
+  verifyServerAvailability(() => {
+    client.emit('remove', id);
+  });
 }
 
 function start(id) {
-  client.emit('start', id);
+  verifyServerAvailability(() => {
+    client.emit('start', id);
+  });
 }
 
 function stop(id) {
-  client.emit('stop', id);
+  verifyServerAvailability(() => {
+    client.emit('stop', id);
+  });
 }
 
 function restart(id) {
-  client.emit('restart', id);
+  verifyServerAvailability(() => {
+    client.emit('restart', id);
+  });
 }
 
 function getStatus(id) {
-  client.emit('status', id);
+  verifyServerAvailability(() => {
+    client.emit('status', id);
 
-  client.on('statusReply', (status) => {
-    printStatus(status);
-    client.disconnect();
+    client.on('statusReply', (status) => {
+      printStatus(status);
+      client.disconnect();
+    });
   });
 }
 
 function getList() {
-  client.emit('list');
+  verifyServerAvailability(() => {
+    client.emit('list');
 
-  client.on('listReply', (list) => {
-    list.map((proc) => {
-      printStatus(proc);
+    client.on('listReply', (list) => {
+      list.map((proc) => {
+        printStatus(proc);
+      });
+      client.disconnect();
     });
-    client.disconnect();
   });
 }
 
@@ -95,13 +127,7 @@ function killProcessManager(callback) {
 
 }
 
-client.on('messageReceived', () => {
-  client.disconnect();
-  process.exit(0);
-});
-
 module.exports = {
-  init,
   add,
   remove,
   start,
